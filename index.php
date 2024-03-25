0a98bfecea7c66e632e46a415a92cbef2f03a695
crystalgeyser-dk <82807101+crystalgeyser-dk@users.noreply.github.com>
2024-03-25 10:25:45 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tatsu's gym</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Tatsu's gym</h1>
+
+        <nav>
+            <ul>
+                <li><a href="index.php" class="nav-link">Home</a></li>
+                <li><a href="registration.html" class="nav-link">Sign Up</a></li>
+                <li><a href="schedule_index.php" class="nav-link">Class Schedule</a></li>
+                <li><a href="login.html" class="nav-link">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section class="hero">
+            <h2>welcom</h2>
+            <p>let's find your best style</p>
+            <a href="registration.html" class="btn">Sign Up</a>
+            <a href="schedule_index.php" class="btn">Class Schedule</a>
+        </section>
+
+        <section class="features">
+            <h2>Features</h2>
+            <div class="feature">
+                <h3>Fitness Class</h3>
+                <p>We offer a variety of fitness classes. Find the one that suits you best.</p>
+            </div>
+            <div class="feature">
+                <h3>Expert trainers</h3>
+                <p>Experienced trainers provide support to help you achieve your goals.</p>
+            </div>
+        </section>
+
+
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Tatsu's website</p>
+    </footer>
+
+    <script src="scripts.js"></script>
+</body>
+</html>
